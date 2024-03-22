563f02091ce09bf1bb17fd9d006ba491024c638c
hategards <75065764+hategards@users.noreply.github.com>
2024-03-22 14:09:38 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-12dda1de-3711-452c-9229-ed4fec6925af/)
 
 
 echo "$curl_output" >> Update.tsx
